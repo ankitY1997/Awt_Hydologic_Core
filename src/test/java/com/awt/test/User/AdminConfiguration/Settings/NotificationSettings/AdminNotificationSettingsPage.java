fbe11ef41808705d041171a30ba0633c518d6470
ankitY1997 <ankiteng1997@gmailcom>
2024-09-12 18:03:58 +0530

    Add methods In Notification Settings

---
+++ src/test/java/com/awt/test/User/AdminConfiguration/Settings/NotificationSettings/AdminNotificationSettingsPage.java
@@ -0,0 +1,18 @@
+package com.awt.test.User.AdminConfiguration.Settings.NotificationSettings;
+
+import org.openqa.selenium.WebDriver;
+
+import com.awt.page.User.AdminConfiguration.AdminPage;
+
+public class AdminNotificationSettingsPage extends AdminPage {
+
+	
+	//**Constructor**//
+	public AdminNotificationSettingsPage(WebDriver driver) {
+		super(driver);
+		
+	}
+	
+	
+
+}
